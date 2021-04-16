08b4d90b05393cd95b6d9c4bc27cea6feaa4d6c7
MCLeo01 <73120779+MCLeo01@users.noreply.github.com>
2021-04-16 22:14:55 +0800

    Loading screen + dialog

---
+++ app/src/main/java/com/example/myapplication/LoadingPage.java
@@ -0,0 +1,65 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+
+public class LoadingPage extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private int progressStatus = 0;
+    boolean firsttime = true;
+
+    private Handler handler = new Handler();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_page);
+
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setMax(150);
+        progressBar.getProgressDrawable().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN);
+        start_progressbar();
+
+    }
+
+
+    public void start_progressbar(){
+        Intent intent = new Intent();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(progressStatus < progressBar.getMax()){
+                    progressStatus++;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                        }
+                    });
+                    try {
+                        Thread.sleep(20);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+                if(progressStatus == progressBar.getMax()){
+                    intent.setClass(LoadingPage.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+            }
+        }).start();
+    }
+
+    @Override
+    public void onBackPressed() {
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            showQuitDialog();
+            return true;
+        }
+        else
+            return false;
+    }
+
+    private void showQuitDialog(){
+        builder = new AlertDialog.Builder(this);
+        builder.setMessage("你要離開嗎?");
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("是", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        builder.setNegativeButton("否", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.setTitle("關閉程式");
+        alert.show();
     }
 }
