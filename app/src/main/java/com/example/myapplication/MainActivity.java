7ccda92beefbb6d0ba80c99ab243a9e0846b6834
MCLeo01 <73120779+MCLeo01@users.noreply.github.com>
2021-04-17 03:52:36 +0800

    main page logo updated

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     private CountDownTimer timer;
     private Boolean blue = true;
     private ImageButton main_play;
+    private ImageView logo1, logo2, logo3,logo4,logo5,logo6,logo7,logo8,logo9,logo10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,15 +61,38 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         rl = findViewById(R.id.rl);
         rl.setOnTouchListener(this);
 
-
+        logo1 = findViewById(R.id.logo1);
+        logo2 = findViewById(R.id.logo2);
+        logo2.setRotation(45);
+        logo3 = findViewById(R.id.logo3);
+        logo3.setRotation(-20);
+        logo4 = findViewById(R.id.logo4);
+        logo5 = findViewById(R.id.logo5);
+        logo5.setRotation(40);
+        logo6 = findViewById(R.id.logo6);
+        logo7 = findViewById(R.id.logo7);
+        logo8 = findViewById(R.id.logo8);
+        logo8.setRotation(20);
+        logo9 = findViewById(R.id.logo9);
+        logo9.setRotation(-22);
+        logo10 = findViewById(R.id.logo10);
+        logo10.setRotation(33);
+        setlocation(logo1, 250, 675);
+        setlocation(logo2, 905, 400);
+        setlocation(logo3, 349, 149);
+        setlocation(logo4, 720, 65);
+        setlocation(logo5, 80, 1175);
+        setlocation(logo6, 896, 921);
+        setlocation(logo7, 928, 1470);
+        setlocation(logo8, 35, 250);
+        setlocation(logo9, 132, 1577);
+        setlocation(logo10, 790, 1629);
 
 
 //==============timer====================\\
         timer = new CountDownTimer(1500, 20) {
-
             @Override
             public void onTick(long millisUntilFinished) {
-
             }
             @Override
             public void onFinish() {
@@ -83,11 +107,14 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 }
             }
         }.start();
-
     }
 
 
 
+    public void setlocation(ImageView logo,float x, float y){
+        logo.setX(x);
+        logo.setY(y);
+    }
 
 
     public void changeYellow(){
@@ -101,6 +128,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         timer.start();
     }
 
+
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if(keyCode == KeyEvent.KEYCODE_BACK){
@@ -110,6 +138,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         else
             return false;
     }
+
 
     private void showQuitDialog(){
         builder = new AlertDialog.Builder(this);
@@ -122,7 +151,6 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 finish();
             }
         });
-
         builder.setNegativeButton("否", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -134,6 +162,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         alert.setTitle("關閉程式");
         alert.show();
     }
+
 
     public void onClick(View v){
         Intent it = new Intent();
@@ -170,13 +199,9 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     public void rocketMove() {
         Animation a = new TranslateAnimation(1000, Animation.ABSOLUTE - 2000,
                 950, Animation.ABSOLUTE - 300);
-
         a.setDuration(10000);
-
         a.setRepeatCount(Animation.INFINITE);
-
         a.setRepeatMode(Animation.RESTART);
-
         rocket.startAnimation(a);
     }
 }
