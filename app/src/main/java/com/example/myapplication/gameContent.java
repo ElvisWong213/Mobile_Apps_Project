422407bf8eb39adb9f22e1356fe92108e4f38364
matthewmak0315 <81973522+matthewmak0315@users.noreply.github.com>
2021-04-24 15:50:28 +0800

    j

--- app/src/main/java/com/example/myapplication/gameContent.java
+++ app/src/main/java/com/example/myapplication/gameContent.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class gameContent extends AppCompatActivity {
@@ -12,7 +13,7 @@ public class gameContent extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_content);
         int checkpoint = 1;
-        ImageButton char1,char2,char3,char4,char5,char6,char7,char8,char9,char10,char11,char12,char13,char14,char15,char16;
+        Button char1,char2,char3,char4,char5,char6,char7,char8,char9,char10,char11,char12,char13,char14,char15,char16;
 
     }
 }
