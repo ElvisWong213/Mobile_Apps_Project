3e5e590fbbc7840564026a3fd71f6413dd4bc974
matthewmak0315 <81973522+matthewmak0315@users.noreply.github.com>
2021-04-24 18:18:35 +0800

    Update gameContent.java

--- app/src/main/java/com/example/myapplication/gameContent.java
+++ app/src/main/java/com/example/myapplication/gameContent.java
@@ -12,7 +12,7 @@ public class gameContent extends AppCompatActivity implements View.OnClickListen
     int checkpoint = 1;
     String chinese = "";
     TextView userinput;
-    Button submit, clear;
+    Button submit, clear,next;
     Button textBtn1,textBtn2,textBtn3,textBtn4,textBtn5,textBtn6,textBtn7,textBtn8,textBtn9,textBtn10,textBtn11,textBtn12,textBtn13,textBtn14;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,77 +67,84 @@ public class gameContent extends AppCompatActivity implements View.OnClickListen
     }
 
     public void onClick(View v){
-       switch (v.getId()) {
-           case R.id.textBtn1:
-               chinese+= textBtn1.getText();
-               userinput.setText(chinese);
-               break;
-           case R.id.textBtn2:
-               chinese+= textBtn2.getText();
-               userinput.setText(chinese);
-               break;
-           case R.id.textBtn3:
-               chinese+= textBtn3.getText();
-               userinput.setText(chinese);
-               break;
-           case R.id.textBtn4:
-               chinese+= textBtn4.getText();
-               userinput.setText(chinese);
-               break;
-           case R.id.textBtn5:
-               chinese+= textBtn5.getText();
-               userinput.setText(chinese);
-               break;
-           case R.id.textBtn6:
-               chinese+= textBtn6.getText();
-               userinput.setText(chinese);
-               break;
-           case R.id.textBtn7:
-               chinese+= textBtn7.getText();
-               userinput.setText(chinese);
-               break;
-           case R.id.textBtn8:
-               chinese+= textBtn8.getText();
-               userinput.setText(chinese);
-               break;
-           case R.id.textBtn9:
-               chinese+= textBtn9.getText();
-               userinput.setText(chinese);
-               break;
-           case R.id.textBtn10:
-               chinese+= textBtn10.getText();
-               userinput.setText(chinese);
-               break;
-           case R.id.textBtn11:
-               chinese+= textBtn11.getText();
-               userinput.setText(chinese);
-               break;
-           case R.id.textBtn12:
-               chinese+= textBtn12.getText();
-               userinput.setText(chinese);
-               break;
-           case R.id.textBtn13:
-               chinese+= textBtn13.getText();
-               userinput.setText(chinese);
-               break;
-           case R.id.textBtn14:
-               chinese+= textBtn14.getText();
-               userinput.setText(chinese);
-               break;
-           case R.id.submit:
-               if(checkpoint==1){
-                   String checkpoint1Ans="你你你你";
-                   if(checkpoint1Ans!=chinese){
-                       userinput.setText("");
-                       chinese="";}
-                   else
-                       userinput.setText("good");
-               }
-           case R.id.clear:
-               userinput.setText("");
+        switch (v.getId()) {
+            case R.id.textBtn1:
+                chinese+= textBtn1.getText();
+                userinput.setText(chinese);
+                break;
+            case R.id.textBtn2:
+                chinese+= textBtn2.getText();
+                userinput.setText(chinese);
+                break;
+            case R.id.textBtn3:
+                chinese+= textBtn3.getText();
+                userinput.setText(chinese);
+                break;
+            case R.id.textBtn4:
+                chinese+= textBtn4.getText();
+                userinput.setText(chinese);
+                break;
+            case R.id.textBtn5:
+                chinese+= textBtn5.getText();
+                userinput.setText(chinese);
+                break;
+            case R.id.textBtn6:
+                chinese+= textBtn6.getText();
+                userinput.setText(chinese);
+                break;
+            case R.id.textBtn7:
+                chinese+= textBtn7.getText();
+                userinput.setText(chinese);
+                break;
+            case R.id.textBtn8:
+                chinese+= textBtn8.getText();
+                userinput.setText(chinese);
+                break;
+            case R.id.textBtn9:
+                chinese+= textBtn9.getText();
+                userinput.setText(chinese);
+                break;
+            case R.id.textBtn10:
+                chinese+= textBtn10.getText();
+                userinput.setText(chinese);
+                break;
+            case R.id.textBtn11:
+                chinese+= textBtn11.getText();
+                userinput.setText(chinese);
+                break;
+            case R.id.textBtn12:
+                chinese+= textBtn12.getText();
+                userinput.setText(chinese);
+                break;
+            case R.id.textBtn13:
+                chinese+= textBtn13.getText();
+                userinput.setText(chinese);
+                break;
+            case R.id.textBtn14:
+                chinese+= textBtn14.getText();
+                userinput.setText(chinese);
+                break;
+            case R.id.submit:
+                if(checkpoint==1){
+                    String checkpoint1Ans="你你你";
 
+                    if(checkpoint1Ans.equals(chinese)){
+                        userinput.setText("good");
+                        chinese="";
+                        DialogSetting.win_DialogManager(gameContent.this);
+                        checkpoint++;
 
-       }
-     }
+                    }
+                    else{
+                        chinese="";
+                        userinput.setText(chinese);}
+                }
+                break;
+            case R.id.clear:
+                userinput.setText("");
+                break;
 
+        }
     }
+
+}
