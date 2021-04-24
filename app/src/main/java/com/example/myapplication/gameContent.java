f1b7b8ab415db96eaf45a37b737efabec2f97f52
matthewmak0315 <81973522+matthewmak0315@users.noreply.github.com>
2021-04-24 16:21:55 +0800

    Update gameContent.java

--- app/src/main/java/com/example/myapplication/gameContent.java
+++ app/src/main/java/com/example/myapplication/gameContent.java
@@ -3,10 +3,11 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-public class gameContent extends AppCompatActivity {
+public class gameContent extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +29,33 @@ public class gameContent extends AppCompatActivity {
         textBtn12 = findViewById(R.id.textBtn12);
         textBtn13 = findViewById(R.id.textBtn13);
         textBtn14 = findViewById(R.id.textBtn14);
+        textBtn1.setOnClickListener(this);
+        textBtn2.setOnClickListener(this);
+        textBtn3.setOnClickListener(this);
+        textBtn4.setOnClickListener(this);
+        textBtn5.setOnClickListener(this);
+        textBtn6.setOnClickListener(this);
+        textBtn7.setOnClickListener(this);
+        textBtn8.setOnClickListener(this);
+        textBtn9.setOnClickListener(this);
+        textBtn10.setOnClickListener(this);
+        textBtn11.setOnClickListener(this);
+        textBtn12.setOnClickListener(this);
+        textBtn13.setOnClickListener(this);
+        textBtn14.setOnClickListener(this);
 
 
+
+
+
+
+
+
+
+
+    }
+
+    public void onClick(View v){
 
 
     }
