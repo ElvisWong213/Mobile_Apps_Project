dd224b94424338343c41475858db064af6133613
matthewmak0315 <81973522+matthewmak0315@users.noreply.github.com>
2021-04-24 16:17:10 +0800

    Update gameContent.java

--- app/src/main/java/com/example/myapplication/gameContent.java
+++ app/src/main/java/com/example/myapplication/gameContent.java
@@ -13,7 +13,24 @@ public class gameContent extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_content);
         int checkpoint = 1;
-        Button char1,char2,char3,char4,char5,char6,char7,char8,char9,char10,char11,char12,char13,char14,char15,char16;
+        Button textBtn1,textBtn2,textBtn3,textBtn4,textBtn5,textBtn6,textBtn7,textBtn8,textBtn9,textBtn10,textBtn11,textBtn12,textBtn13,textBtn14;
+        textBtn1 = findViewById(R.id.textBtn1);
+        textBtn2 = findViewById(R.id.textBtn2);
+        textBtn3 = findViewById(R.id.textBtn3);
+        textBtn4 = findViewById(R.id.textBtn4);
+        textBtn5 = findViewById(R.id.textBtn5);
+        textBtn6 = findViewById(R.id.textBtn6);
+        textBtn7 = findViewById(R.id.textBtn7);
+        textBtn8 = findViewById(R.id.textBtn8);
+        textBtn9 = findViewById(R.id.textBtn9);
+        textBtn10 = findViewById(R.id.textBtn10);
+        textBtn11 = findViewById(R.id.textBtn11);
+        textBtn12 = findViewById(R.id.textBtn12);
+        textBtn13 = findViewById(R.id.textBtn13);
+        textBtn14 = findViewById(R.id.textBtn14);
+
+
+
 
     }
 }
