2877e67bda9ca5162591c29a565eacfe2cbe919b
matthewmak0315 <81973522+matthewmak0315@users.noreply.github.com>
2021-04-24 16:27:42 +0800

    Update gameContent.java

--- app/src/main/java/com/example/myapplication/gameContent.java
+++ app/src/main/java/com/example/myapplication/gameContent.java
@@ -8,13 +8,14 @@ import android.widget.Button;
 import android.widget.ImageButton;
 
 public class gameContent extends AppCompatActivity implements View.OnClickListener{
-
+    int checkpoint = 1;
+    Button textBtn1,textBtn2,textBtn3,textBtn4,textBtn5,textBtn6,textBtn7,textBtn8,textBtn9,textBtn10,textBtn11,textBtn12,textBtn13,textBtn14;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_content);
-        int checkpoint = 1;
-        Button textBtn1,textBtn2,textBtn3,textBtn4,textBtn5,textBtn6,textBtn7,textBtn8,textBtn9,textBtn10,textBtn11,textBtn12,textBtn13,textBtn14;
+
+
         textBtn1 = findViewById(R.id.textBtn1);
         textBtn2 = findViewById(R.id.textBtn2);
         textBtn3 = findViewById(R.id.textBtn3);
@@ -56,7 +57,9 @@ public class gameContent extends AppCompatActivity implements View.OnClickListen
     }
 
     public void onClick(View v){
-
+     if(checkpoint==1){
+         
+     }
 
     }
 }
