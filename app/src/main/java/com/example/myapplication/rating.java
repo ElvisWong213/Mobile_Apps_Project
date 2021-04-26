2b58074c7b1eabe8837de55a880f57db1722c36b
kuroko1823 <73170020+kuroko1823@users.noreply.github.com>
2021-04-26 21:39:54 +0800

    custom dialog box for rating

--- app/src/main/java/com/example/myapplication/rating.java
+++ app/src/main/java/com/example/myapplication/rating.java
@@ -2,9 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
@@ -13,11 +16,42 @@ import android.widget.Toast;
 public class rating extends AppCompatActivity {
     RatingBar ratingBar;
     ImageView btSubmit;
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
+
+        dialog = new Dialog(rating.this);
+        dialog.setContentView(R.layout.activity_feedback_dialog_box);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.feedback_dialog_resource));
+        }
+
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(false);
+        dialog.getWindow().getAttributes().windowAnimations = R.style.animation_dialog;
+
+        Button yes = dialog.findViewById(R.id.btn_right);
+        Button no = dialog.findViewById(R.id.btn_left);
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "已送出評分", Toast.LENGTH_SHORT).show();
+                Intent it = new Intent();
+                it.setClass(rating.this,gamePage.class);
+                startActivity(it);
+                dialog.dismiss();
+            }
+        });
+
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
 
         ratingBar = findViewById(R.id.rating_bar);
         btSubmit = findViewById(R.id.ratebtnswitch);
@@ -25,11 +59,7 @@ public class rating extends AppCompatActivity {
         btSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String s = String.valueOf(ratingBar.getRating());
-                Toast.makeText(getApplicationContext(), "已送出評分", Toast.LENGTH_SHORT).show();
-                Intent it = new Intent();
-                it.setClass(rating.this,gamePage.class);
-                startActivity(it);
+                dialog.show();
             }
         });
     }
