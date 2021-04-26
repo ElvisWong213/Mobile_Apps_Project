703fa4add2ad4dfd437d24eb03cde8094c4ea812
MCLeo01 <73120779+MCLeo01@users.noreply.github.com>
2021-04-26 22:04:57 +0800

    test

--- app/src/main/java/com/example/myapplication/DialogSetting.java
+++ app/src/main/java/com/example/myapplication/DialogSetting.java
@@ -65,6 +65,7 @@ public class DialogSetting{
                 if(pref.getBoolean("effectsound",true) == true)
                     mediaPlayer.start();
                 if(page.getClass()!=MainActivity.class || (page.getClass()==MainActivity.class)) {
+                    rating.currentPage = page.getClass();
                     it.setClass(page, rating.class);
                     page.startActivity(it);
                     CustomIntent.customType(page, "bottom-to-up");
