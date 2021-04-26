e10d081f103dfe7d632383a9e615062576f5347d
MCLeo01 <73120779+MCLeo01@users.noreply.github.com>
2021-04-26 23:30:28 +0800

    Update rating.java

--- app/src/main/java/com/example/myapplication/rating.java
+++ app/src/main/java/com/example/myapplication/rating.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -61,6 +63,20 @@ public class rating extends AppCompatActivity {
 
         ratingBar = findViewById(R.id.rating_bar);
         btSubmit = findViewById(R.id.ratebtnswitch);
+        btSubmit.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if (DialogSetting.effectsoundcontrol(getApplicationContext())){
+                        mediaPlayer.start();
+                    }
+                    btSubmit.setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    btSubmit.clearColorFilter();
+                }
+                return false;
+            }
+        });
 
         btSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
